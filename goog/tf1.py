6c9604cb9cb76308685971ba9454643dfdc04e7c
Ubuntu <ubuntu@ip-172-31-91-97.ec2.internal>
2018-12-24 16:22:02 +0000

    goog

---
+++ goog/tf1.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+classifier = tf.estimator.LinearClassifier(feature_columns)
+
+classifier.train(input_fn=train_input_fn, steps=2000)
+
+predictions = classifier.predict(input_fn=predict_input_fn)
+
